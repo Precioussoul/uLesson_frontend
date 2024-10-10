c62d7d7849be0659143401cb2dd567aef974f736
Precioussoul <habsoff@gmail.com>
2024-10-10 18:25:17 +0100

    feat:  working on edit student

---
+++ src/components/EditStudentModal.tsx
@@ -0,0 +1,61 @@
+"use client"
+import React from "react"
+import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Input} from "@chakra-ui/react"
+
+interface EditStudentModal {
+  onClose: () => void
+  isOpen: boolean
+}
+
+const EditStudentModal = ({isOpen, onClose}: EditStudentModal) => {
+  return (
+    <Modal isCentered isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add New Student</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <form>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
+                Name
+              </label>
+              <Input type='text' placeholder='Enter first name and last name' />
+            </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='registrationNumber'>
+                Registration Number
+              </label>
+              <Input type='text' placeholder='Enter registration number' />
+            </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='major'>
+                Major
+              </label>
+              <Input type='text' placeholder='e.g Computer Engineering ' />
+            </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='dob'>
+                Date of Birth
+              </label>
+              <Input type='date' />
+            </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='gpa'>
+                GPA
+              </label>
+              <Input type='number' placeholder='Enter GPA' />
+            </div>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none  ' type='button' onClick={onClose}>
+            Save
+          </button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default EditStudentModal
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,8 @@
 "use client"
-import React from "react"
+import React, {useState} from "react"
 import {Table, Thead, Tbody, Tr, Th, Td, TableContainer} from "@chakra-ui/react"
 import {MdDelete, MdEdit, MdEditNote, MdOutlineDelete, MdOutlineEdit} from "react-icons/md"
+import EditStudentModal from "./EditStudentModal"
 
 interface Student {
   id: string
@@ -19,48 +20,54 @@ interface StudentsTableProps {
 }
 
 const StudentsTable = ({data}: StudentsTableProps) => {
+  const [isOpen, setIsOpen] = useState(false)
   const handleStudentDelete = (id: string | number) => {}
-  const handleEditStudent = (id: string | number) => {}
+  const handleEditStudent = (id: string | number) => {
+    setIsOpen(true)
+  }
 
   console.log("Student", data)
 
   return (
-    <TableContainer>
-      <Table variant='simple'>
-        <Thead backgroundColor={"gray.200"}>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Registration Number</Th>
-            <Th>Major</Th>
-            <Th>Date of Birth</Th>
-            <Th>GPA</Th>
-            <Th>Actions</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.data.length > 0 &&
-            data.data.map((student, idx) => (
-              <Tr key={idx}>
-                <Td>{student.name}</Td>
-                <Td>{student.registrationNumber}</Td>
-                <Td>{student.major}</Td>
-                <Td>{student.dob}</Td>
-                <Td>{student.gpa}</Td>
-                <Td>
-                  <div className='flex items-center gap-4'>
-                    <div className='flex items-center justify-center border border-gray-200 rounded-lg p-2'>
-                      <MdOutlineEdit size={20} onClick={() => handleEditStudent(student.id)} />
+    <>
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead backgroundColor={"gray.200"}>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Registration Number</Th>
+              <Th>Major</Th>
+              <Th>Date of Birth</Th>
+              <Th>GPA</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.data.length > 0 &&
+              data.data.map((student, idx) => (
+                <Tr key={idx}>
+                  <Td>{student.name}</Td>
+                  <Td>{student.registrationNumber}</Td>
+                  <Td>{student.major}</Td>
+                  <Td>{student.dob}</Td>
+                  <Td>{student.gpa}</Td>
+                  <Td>
+                    <div className='flex items-center gap-4'>
+                      <div className='flex items-center justify-center border border-gray-200 rounded-lg p-2'>
+                        <MdOutlineEdit size={20} onClick={() => handleEditStudent(student.id)} />
+                      </div>
+                      <div className='flex items-center justify-center border border-gray-200 rounded-lg p-2'>
+                        <MdOutlineDelete size={20} onClick={() => handleStudentDelete(student.id)} />
+                      </div>
                     </div>
-                    <div className='flex items-center justify-center border border-gray-200 rounded-lg p-2'>
-                      <MdOutlineDelete size={20} onClick={() => handleStudentDelete(student.id)} />
-                    </div>
-                  </div>
-                </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <EditStudentModal isOpen={isOpen} onClose={() => setIsOpen((prev) => !prev)} />
+    </>
   )
 }
 
