18c688e01ad4f9280341a275cac457e3ad7821d8
Precioussoul <habsoff@gmail.com>
2024-10-11 01:28:59 +0100

    feat:  wrap up

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server"
-//TODO: starter code for API configuration
+//Tips: starter code for API configuration
 export async function GET(req: Request) {
   return NextResponse.json({message: "Hello world"})
 }
